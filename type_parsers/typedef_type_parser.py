4fabe32a389c5c911fb20679ca19addf3811cbda
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2025-06-18 19:09:53 +0300

    fix: aliases

--- type_parsers/typedef_type_parser.py
+++ type_parsers/typedef_type_parser.py
@@ -2,11 +2,6 @@ import dataclasses
 import interface_type_parser
 
 @dataclasses.dataclass
-class InterfaceTypeConcrete:
-    referenced_type : str
-
-
-@dataclasses.dataclass
 class TypedefAliasSimple:
     alias_key: str
     alias_value: interface_type_parser.InterfaceTypeSimple
